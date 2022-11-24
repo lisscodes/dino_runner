707928d6565e60e16f9d726dcff5c3ec257d9680
lisscodes <lisandra.dias008@gmail.com>
2022-11-23 22:30:20 -0300

    AULA 3

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -1,18 +1,17 @@
-from dino_runner.components.obstacles.obstacle import Obstacle
 from dino_runner.utils.constants import BIRD
+from dino_runner.components.obstacles.obstacle import Obstacle
 
-Y_POS = 250
 
 class Bird(Obstacle):
-    def __init__(self, image):
-        self.type = 0
-        super().__init__(image, self.type)
-        self.bird_rect = self.image.get_rect()
-        self.bird_rect.y = Y_POS
+    def __init__(self):
+        super().__init__(BIRD, 0)
+        self.rect.y = 250
         self.step_index = 0
 
     def draw(self, screen):
-        if self.step_index >= 9:
+        screen.blit(self.image[self.step_index // 5], self.rect)
+        self.step_index += 1
+
+        if self.step_index >= 10:
             self.step_index = 0
-        screen.blit(self.image[self.step_index//5], self.rect)
         self.step_index += 1
